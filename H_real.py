86175476766aff9ada98fae93789b708a8f15001
Leonardo Benini <phrgmr@vonderheydt.theory.warwick.ac.uk>
2018-10-23 16:19:22 +0100

    Sparse version initial commit

--- H_real.py
+++ H_real.py
@@ -4,15 +4,17 @@ import scipy.special as special
 import H_functions as hf
 import H_ent as ent
 
-def ExDiag(PATH_now,L,D):
+def ExDiag(PATH_now,L,D,Jzz):
 	t0=time.time()
 
-	#### BOUNDARY CONDITIONS (0 ---> open, 1 ---> periodic) ###
-	BC = 0
+	#### BOUNDARY CONDITIONS (0 ---> periodic, 1 ---> open) ###
+	BC = 1
 
 	### DISORDER FLAG (0 ---> random, 1 ---> quasiperiodic) ###
 	Dis_gen = 0
 
+	### FORMAT FLAG (0 ---> dense, 1 ---> sparse) ###
+	form_flag = 1
 	### HILBERT SPACE DIMENSION ###
 	LL = int(L)
 	DD = float(D)
@@ -40,20 +42,25 @@ def ExDiag(PATH_now,L,D):
 	Dis_real = hf.Dis_Creation(LL,Dis_gen)
 
 	### CREATION OF HAMILTONIAN MATRICES ###
-	HAM = hf.Ham_Dense_Creation(LL,NN,Dim,DD,Dis_real,BC,Base_bin,Base_num,Hop_bin,LinTab)
+	if form_flag == 0:
+		HAM = hf.Ham_Dense_Creation(LL,NN,Dim,DD,Jzz,Dis_real,BC,Base_bin,Base_num,Hop_bin,LinTab)
+	else:
+		HAM = hf.Ham_Sparse_Creation(LL,NN,Dim,DD,Jzz,Dis_real,BC,Base_bin,Base_num,Hop_bin,LinTab)
+
 
 	#### DIAGONALIZATION ###
-	Eval, Evec = hf.eigval(HAM)
+	Eval, Evec = hf.eigval(HAM, Dim)
+	#Eval_q, Evec_q = hf.eigval(HAM_q)
 
 	### NORMALIZE SPECTRUM ###
 	E_norm = (Eval[1:]-min(Eval[1:]))/(max(Eval[1:])- min(Eval[1:]))
 
 	### LEVEL STATISTICS (MIDDLE OF THE SPECTRUM) ###
-	#Eval_mid = E_norm[int(Dim/3):int(2*Dim/3)]
-	#ravg = hf.levstat(Eval_mid)
-	#nomefile_lev = str(PATH_now+'Levst_L'+str(LL)+'_D'+str(D)+'.dat')
-	#with open(nomefile_lev, 'a') as ee:
-	#	ee.write('%f' % ravg +"\n")
+	Eval_mid = E_norm[int(Dim/3):int(2*Dim/3)]
+	ravg = hf.levstat(Eval_mid)
+	nomefile_lev = str(PATH_now+'Levst_L'+str(LL)+'_D'+str(D)+'.dat')
+	with open(nomefile_lev, 'a') as ee:
+		ee.write('%f' % ravg +"\n")
 
 	### INVERSE PARTICIPATION RATIO ###
 	#ipr = hf.InvPartRatio(Evec)
@@ -63,7 +70,7 @@ def ExDiag(PATH_now,L,D):
 	#with open(nomefile_ipr, 'a') as ee:
 	#	ee.write('%f' % avg_ipr % "\n")
 
-	in_flag = 2
+	in_flag = 0
 
 	Psi0 = hf.Psi_0(Dim, LL, Base_num, in_flag)
 	ProjPsi0 = hf.Proj_Psi0(Psi0, Evec)
@@ -72,27 +79,29 @@ def ExDiag(PATH_now,L,D):
 	t_i   = float(0.0)
 	t_f   = float(10.0)
 	Nstep = int(100)
-	tmp_tab = np.linspace(t_i, t_f, Nstep)
-	#t_tab = np.sort(np.append(tmp_tab, [0, 0.25, 0.5, 0.75]))
+	tmp_tab = np.logspace(t_i, t_f, Nstep)
+	t_tab = np.sort(np.append(tmp_tab, [0, 0.25, 0.5, 0.75]))
 
 	for t in tmp_tab:
 		Psit = hf.TimEvolve(ProjPsi0, Eval, t)
 
-		#Losch = hf.Loschmidt(Psit, ProjPsi0)
-		#nomefile_losch = str(PATH_now+'Losch_t' + str(t) + '.dat')
-		#with open(nomefile_losch, 'a') as file:
-		#	file.write('%f' % Losch +"\n")
+		Losch = hf.Loschmidt(Psit, ProjPsi0)
+		nomefile_losch = str(PATH_now+'Losch_t' + str(t) + '.dat')
+		with open(nomefile_losch, 'a') as file:
+			file.write('%f' % Losch +"\n")
 
-		#VnEnt = ent.compute_entanglement_entropy(Psit, LL, NN, NN)
-		#nomefile_ent = str(PATH_now+'Ent_t' + str(t) + '.dat')
-		#with open(nomefile_ent, 'a') as file:
-		#	file.write('%f' % VnEnt +"\n")
+		# VnEnt = ent.compute_entanglement_entropy(Psit, LL, NN, NN)
+		# nomefile_ent = str(PATH_now+'Ent_t' + str(t) + '.dat')
+		# with open(nomefile_ent, 'a') as file:
+		# 	file.write('%f' % VnEnt +"\n")
+		#
+		# Exp_Sz, Tot_Sz = hf.magnetization(Psit, Base_NumRes)
+		# nomefile_mag_profile = str(PATH_now+'Mag_t' + str(t) + '.dat')
+		# nomefile_totmag = str(PATH_now+'SzHalf_t' + str(t) + '.dat')
+		# with open(nomefile_mag_profile, 'w') as file:
+		# 	for i in range(len(Exp_Sz)):
+		# 		file.write('%f' % np.real(Exp_Sz[i]) +'   %i' % int(i+1) + '   %f'% t +'   %f' %np.sum(np.real(Exp_Sz))+"\n")
+		# with open(nomefile_totmag, 'w') as file:
+		# 	file.write('%f' % Tot_Sz + '\n')
 
-		Exp_Sz, Tot_Sz = hf.magnetization(Psit, Base_NumRes)
-		nomefile_mag_profile = str(PATH_now+'Mag_t' + str(t) + '.dat')
-		nomefile_totmag = str(PATH_now+'SzHalf_t' + str(t) + '.dat')
-		with open(nomefile_mag_profile, 'w') as file:
-			for i in range(len(Exp_Sz)):
-				file.write('%f' % np.real(Exp_Sz[i]) +'   %i' % int(i+1) + '   %f'% t +'   %f' %np.sum(np.real(Exp_Sz))+"\n")
-		with open(nomefile_totmag, 'w') as file:
-			file.write('%f' % Tot_Sz + '\n')
+	return 1
